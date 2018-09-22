9b5104f793b218ddbcb301a1f976c933b2d9d897
shpulya <i.brichak@gmail.com>
2018-09-22 18:08:52 +0300

    add functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MaterialModule} from './material/material.module';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ZipInfoComponent } from './zip-info/zip-info.component';
+import {ZipService} from './zip.service';
+import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    ZipInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ZipService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
